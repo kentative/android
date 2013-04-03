0925ba552651f24b636f9d5791ed2ebe5bc26629
Kent Lam <kentative@live.com>
2013-04-02 21:18:29 -0700

    Release update

--- HelloWorld/src/com/raytheon/hello/MainActivity.java
+++ HelloWorld/src/com/raytheon/hello/MainActivity.java
@@ -1,8 +1,14 @@
 package com.raytheon.hello;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,12 +16,30 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        Bundle extras = this.getIntent().getExtras();
-        extras.get(ExtraId.UserName.name());
-        
-        
         setContentView(R.layout.activity_main);
+        
+        Button byeBtn = (Button) findViewById(R.id.byeBtn);
+        byeBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, Lab01Activity.class));
+			}
+			
+		});
+        
+        // Greets user
+        Bundle extras = this.getIntent().getExtras();
+        Object username = extras.get(ExtraId.UserName.name());
+        
+        Resources resc = getResources();
+        if (username == null || username.toString().length() == 0) {
+        	username = resc.getString(R.string.unknown_user);
+        }
+        
+        TextView greetingsTextView = (TextView) this.findViewById(R.id.greetingsTextView);
+        greetingsTextView.setText(
+    		resc.getString(R.string.greetings) + " " + username);
     }
 
 
